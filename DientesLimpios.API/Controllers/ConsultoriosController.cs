be2d6fbb02e5253c96b85323ff21a87281654804
Ismael Quiroz Santes <ismaelqzs@gmail.com>
2026-06-28 10:16:12 -0600

    Excepciones en Middleware
    manejo de Excepciones en Middleware

--- DientesLimpios.API/Controllers/ConsultoriosController.cs
+++ DientesLimpios.API/Controllers/ConsultoriosController.cs
@@ -1,5 +1,6 @@
 ﻿using DientesLimpios.API.DTOs.Consultorios;
 using DientesLimpios.Aplicacion.CasosDeUso.Consultorios.Comandos.CrearConsultorio;
+using DientesLimpios.Aplicacion.CasosDeUso.Consultorios.Consultas.ObtenerDetalleConsultorio;
 using DientesLimpios.Aplicacion.Utilidades.Mediador;
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,6 +14,14 @@ namespace DientesLimpios.API.Controllers
         public ConsultoriosController(IMediator mediator)
         {
             this.mediator = mediator;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ConsultorioDetalleDTO>> Get(Guid id)
+        {
+            var consulta = new ConsultaObtenerDetalleConsultorio { Id  = id };
+            var resultado = await mediator.Send(consulta);
+            return resultado;
         }
 
         [HttpPost]
---
+++ DientesLimpios.API/Middlewares/ManejadorExcepcionesMiddleware.cs
@@ -0,0 +1,64 @@
+﻿using DientesLimpios.Aplicacion.Excepciones;
+using System.Net;
+using System.Text.Json;
+
+namespace DientesLimpios.API.Middlewares
+{
+    public class ManejadorExcepcionesMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ManejadorExcepcionesMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                //significa invocar al siguiente Middleware, es decir cuado no hubo ningun error
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await ManejarExcepcion(context, ex);
+            }
+        }
+
+        //logica de manejo de errores
+        private Task ManejarExcepcion(HttpContext context, Exception exception)
+        {
+            //Respuesta por defecto que se le enviará al cliente, es un error 500
+            HttpStatusCode httpStatusCode = HttpStatusCode.InternalServerError;
+            context.Response.ContentType = "application/json";
+            var resultado = string.Empty;
+
+            switch (exception)
+            {
+                case ExcepcionNoEncontrado:
+                    httpStatusCode = HttpStatusCode.NotFound;
+                    break;
+                case ExcepcionDeValidacion excepcionDeValidacion:
+                    httpStatusCode = HttpStatusCode.BadRequest;
+                    resultado = JsonSerializer.Serialize(excepcionDeValidacion.ErroresValidacion);
+                    break;
+
+            }
+
+            context.Response.StatusCode = (int)httpStatusCode;
+            return context.Response.WriteAsync(resultado); //para escribir la respuesta hacia el cliente
+        }
+
+    }
+
+    //Se crea una clase estatica para faciliar el uso de este middleware
+    //solo es un metodo auxiliar
+    public static class ManejadorExcepcionesMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseManejadorExcepciones(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ManejadorExcepcionesMiddleware>();
+        }
+    }
+}

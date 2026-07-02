8260fb60db834a268baea237b058a275b50ff527
Ismael Quiroz Santes <ismaelqzs@gmail.com>
2026-07-02 15:15:38 -0600

    BorrarConsultorio
    Implementación de caso de uso y pruebas

--- DientesLimpios.API/Controllers/ConsultoriosController.cs
+++ DientesLimpios.API/Controllers/ConsultoriosController.cs
@@ -1,5 +1,6 @@
 ﻿using DientesLimpios.API.DTOs.Consultorios;
 using DientesLimpios.Aplicacion.CasosDeUso.Consultorios.Comandos.ActualizarConsultorio;
+using DientesLimpios.Aplicacion.CasosDeUso.Consultorios.Comandos.BorrarConsultorio;
 using DientesLimpios.Aplicacion.CasosDeUso.Consultorios.Comandos.CrearConsultorio;
 using DientesLimpios.Aplicacion.CasosDeUso.Consultorios.Consultas.ObtenerDetalleConsultorio;
 using DientesLimpios.Aplicacion.CasosDeUso.Consultorios.Consultas.ObtenerListadoConsultorios;
@@ -50,7 +51,15 @@ namespace DientesLimpios.API.Controllers
         {
             var comando = new ComandoActualizarConsultorio {Id = id,  Nombre = actualizarConsultorioDto.Nombre };
             await mediator.Send(comando);
-            return Ok();
+            return Ok(); //aqui tambien peude ser un NoContent()
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var comando = new ComandoBorrarConsultorio { Id = id };
+            await mediator.Send(comando);
+            return NoContent();
         }
     }
 }
